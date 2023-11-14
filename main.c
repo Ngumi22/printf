3321b007b4a637f329aceae8a3588c38197e64ef
Richkidcode <kamaufelix741@gmail.com>
2023-11-13 22:18:24 -0800

    Made some changes

--- main.c
+++ main.c
@@ -5,18 +5,37 @@
 int _printf(const char *format, ...) {
     va_list args;
     int count = 0;
+    void *addr;
 
     va_start(args, format);
 
     while (*format != '\0') {
         if (*format == '%' && *(format + 1) != '\0') {
-            format++;
+            format++; 
             switch (*format) {
                 case 'c':
                     count += putchar(va_arg(args, int));
                     break;
                 case 's':
                     count += printf("%s", va_arg(args, char *));
+                    break;
+                case 'd':
+                case 'i':
+                    count += printf("%d", va_arg(args, int));
+                    break;
+                case 'u':
+                    count += printf("%u", va_arg(args, unsigned int));
+                    break;
+                case 'o':
+                    count += printf("%o", va_arg(args, unsigned int));
+                    break;
+                case 'x':
+                case 'X':
+                    count += printf("%x", va_arg(args, unsigned int));
+                    break;
+                case 'p':
+                    addr = va_arg(args, void *);
+                    count += printf("%p", addr);
                     break;
                 case '%':
                     count += putchar('%');
