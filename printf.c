323d3ba16586d1184258dd92e39e0657bba48a4a
Richkidcode <kamaufelix741@gmail.com>
2023-11-14 23:35:06 -0800

    Modified printf.c for question 1

--- printf.c
+++ printf.c
@@ -12,43 +12,47 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+    va_list args;
+    int count = 0;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format != '\0')
-	{
-		if (*format == '%' && *(format + 1) != '\0')
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					putchar(va_arg(args, int));
-					count++;
-					break;
-				case 's':
-					count += printf("%s", va_arg(args, char *));
-					break;
-				case '%':
-					putchar('%');
-					count++;
-					break;
-				default:
-					putchar('%');
-					putchar(*format);
-					count += 2;
-					break;
-			}
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+    while (*format != '\0')
+    {
+        if (*format == '%' && *(format + 1) != '\0')
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    putchar(va_arg(args, int));
+                    count++;
+                    break;
+                case 's':
+                    count += printf("%s", va_arg(args, char *));
+                    break;
+                case 'd':
+                case 'i':
+                    count += printf("%d", va_arg(args, int));
+                    break;
+                case '%':
+                    putchar('%');
+                    count++;
+                    break;
+                default:
+                    putchar('%');
+                    putchar(*format);
+                    count += 2;
+                    break;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            count++;
+        }
+        format++;
+    }
+    va_end(args);
+    return (count);
 }
